d5c0f18376b4b57ca399251f41c3abcddfba2443
elmaxe <axel.elmarsson@gmail.com>
2021-04-20 13:55:23 +0200

    Implement function and tests

--- src/app.py
+++ src/app.py
@@ -1,4 +1,22 @@
+import random
 
+default = ["Meh", "Boring", "What about it?", "Nothing special"]
+present = 2021
 
-def some_func():
-    return 0
+def review(year):
+    if year > present:
+        raise ValueError("Can't review a year that has not happened yet (year > {x})".format(x = present))
+    if year == 0:
+        return "Jesus Christ what a year!"
+    elif year == 42:
+        return "A year worth living for"
+    elif year == 1337:
+        return ":sunglasses:"
+    elif year == 1984:
+        return "You never felt alone"
+    elif year == 1987:
+        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    elif year == 2020:
+        return "Sad year :("
+    else:
+        return default[random.randint(0, len(default) - 1)]
--- test/test_app.py
+++ test/test_app.py
@@ -1,11 +1,26 @@
 import unittest
-from src.app import some_func
-
+from src.app import review, default, present
 
 class TestApp(unittest.TestCase):
-    "Blabla"
 
-    def test_some_func(self):
-        "desc"
+    def test_review_should_return_default(self):
+        self.assertIn(review(1), default)
+        self.assertIn(review(-10), default)
+        self.assertIn(review(2021), default)
 
-        self.assertEqual(0, some_func())
+    def test_review_should_not_return_default(self):
+        with self.subTest(msg="year = 0"):
+            self.assertEqual(review(0), "Jesus Christ what a year!")
+        with self.subTest(msg="year = 42"):
+            self.assertEqual(review(42), "A year worth living for")
+        with self.subTest(msg="year = 1337"):
+            self.assertEqual(review(1337), ":sunglasses:")
+        with self.subTest(msg="year = 1984"):
+            self.assertEqual(review(1984), "You never felt alone")
+        with self.subTest(msg="year = 1987"):
+            self.assertEqual(review(1987), "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        with self.subTest(msg="year = 2020"):
+            self.assertEqual(review(2020), "Sad year :(")
+        
+    def test_review_future_year_raise(self):
+        self.assertRaises(ValueError, review, present + 1)
